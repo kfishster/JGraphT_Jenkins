22b2de2bc8be29ffa12cd756eaf8071a2e66ec48
Kirill Varshavskiy <kirillvar@gmail.com>
2015-03-16 18:23:40 -0500

    Automated commit with jenkins trigger

--- commit_and_notify.sh
+++ commit_and_notify.sh
@@ -1,3 +1,4 @@
 git add *
 git commit -m "Automated commit with jenkins trigger"
+git push origin
 curl http://cs498dm-39a.cs.illinois.edu:8080/git/notifyCommit\?url\=https://github.com/kfishster/JGraphT_Jenkins.git
